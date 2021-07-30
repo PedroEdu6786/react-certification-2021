393793b2b4e512b20e9f89d0e402d2f7b5df23f0
PedroEdu6786 <pedrcg835@gmail.com>
2021-07-30 12:58:52 -0500

    :art: feat: add code review refactoring PR 1
    Extracted map function into outside renderVideoItem function

--- src/components/PreviewList/PreviewList.component.jsx
+++ src/components/PreviewList/PreviewList.component.jsx
@@ -3,17 +3,19 @@ import VideoPreview from '../VideoPreview';
 import videos from '../../mocks/videos.json';
 import { PreviewsContainer } from './PreviewList.styles';
 
+const renderVideoItem = (video) => (
+  <VideoPreview
+    key={`${video.id.videoId}${video.snippet.channelId}`}
+    title={video.snippet.title}
+    description={video.snippet.description}
+    thumbnail={video.snippet.thumbnails}
+  />
+);
+
 function PreviewList() {
   return (
     <PreviewsContainer data-testid="preview-list" id="preview-list">
-      {videos.items.map((video) => (
-        <VideoPreview
-          key={`${video.id.videoId}${video.snippet.channelId}`}
-          title={video.snippet.title}
-          description={video.snippet.description}
-          thumbnail={video.snippet.thumbnails}
-        />
-      ))}
+      {videos.items.map(renderVideoItem)}
     </PreviewsContainer>
   );
 }
