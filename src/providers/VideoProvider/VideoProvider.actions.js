aeba96be0d654ec7416564583cdf07500c8e27c8
PedroEdu6786 <pedrcg835@gmail.com>
2021-08-21 13:30:35 -0500

    :sparkles: feat set_favorite_videos reducer, VideoProvider
    add reducer and action to set localStorage videos

--- src/providers/VideoProvider/VideoProvider.actions.js
+++ src/providers/VideoProvider/VideoProvider.actions.js
@@ -1,4 +1,14 @@
-import { SET_INPUT, SET_VIDEOS } from './VideoProvider.types';
+import { SET_FAVORITE_VIDEOS, SET_INPUT, SET_VIDEOS } from './VideoProvider.types';
+
+// returns list of favorite videos from localStorage
+export const addFavoriteVideo = (videoData) => {
+  let favoriteList = localStorage.getItem('REACT-CHALLENGE-FAVORITE-VIDEOS') || [];
+  favoriteList = favoriteList.length > 0 ? JSON.parse(favoriteList) : [];
+  favoriteList.push(videoData);
+  localStorage.setItem('REACT-CHALLENGE-FAVORITE-VIDEOS', JSON.stringify(favoriteList));
+
+  return favoriteList;
+};
 
 export const setInputAction = (value) => {
   return {
@@ -10,6 +20,13 @@ export const setInputAction = (value) => {
 export const setVideosAction = (value) => {
   return {
     type: SET_VIDEOS,
+    payload: value,
+  };
+};
+
+export const setFavoriteVideosAction = (value) => {
+  return {
+    type: SET_FAVORITE_VIDEOS,
     payload: value,
   };
 };
