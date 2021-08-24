33f31310b58236a3e2553c1e6d943e015dc5023b
PedroEdu6786 <pedrcg835@gmail.com>
2021-08-24 18:36:07 -0500

    :white_check_mark: feat: add test for private routes

---
+++ src/components/PrivateRoute/PrivateRoute.component.test.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import { MemoryRouter } from 'react-router';
+import { queryByText, render } from '@testing-library/react';
+import PrivateRoute from './PrivateRoute.component';
+import { Heading } from '../../theme/components/Foundation.component';
+
+jest.mock('@auth0/auth0-react');
+
+const build = () => {
+  const { container } = render(
+    <PrivateRoute>
+      <Heading>Test</Heading>
+    </PrivateRoute>,
+    { wrapper: MemoryRouter }
+  );
+
+  return {
+    container,
+    heading: () => queryByText(container, /Test/i),
+  };
+};
+
+describe('Private route component', () => {
+  it('should not display heading when not authenticated', () => {
+    useAuth0.mockImplementationOnce(() => ({ isAuthenticated: false }));
+    const { heading } = build();
+    expect(heading()).not.toBeInTheDocument();
+  });
+
+  it('should display heading when authenticated', () => {
+    useAuth0.mockImplementationOnce(() => ({ isAuthenticated: true }));
+    const { heading } = build();
+    expect(heading()).toBeInTheDocument();
+  });
+});
